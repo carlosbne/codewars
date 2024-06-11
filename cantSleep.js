db10ab665da134002af21ab26640ee3b0d34e191
carlosbne <106997150+carlosbne@users.noreply.github.com>
2024-06-11 14:58:44 -0300

    11/06/2024 new att

---
+++ cantSleep.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/5b077ebdaf15be5c7f000077
+
+var countSheep = function (num){
+    let str = "";
+    for(let i = 1; i <= num; i++){
+        str += `${[i]} sheep...`;
+    }
+    return str;
+}
+
