3f047e683dbd5ec98cc76873a5c4c0e85d311ce3
carlosbne <106997150+carlosbne@users.noreply.github.com>
2024-06-26 15:34:38 -0300

    new

---
+++ base-math/isTriangle.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/56606694ec01347ce800001b
+
+function isTriangle(a,b,c){
+    if(a + b > c && a + c > b && b + c > a)
+      return true;
+      
+    return false;
+ }
