47ebf037a97db95e215ec0f9d097d44f225b53ed
carlosbne <106997150+carlosbne@users.noreply.github.com>
2024-06-28 16:52:42 -0300

    28/06/24 new att

---
+++ strings/stringToNumber..js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/544675c6f971f7399a000e79
+
+const stringToNumber = function(str){
+    return +str;
+  }
+
+
+var stringToNumber1 = function(str){
+    // put your code here
+    return Number(str);
+  }
