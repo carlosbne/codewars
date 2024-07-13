5b15a90179382127bae986d7c9cfae6060c01bcb
carlosbne <106997150+carlosbne@users.noreply.github.com>
2024-07-13 11:06:42 -0300

    13/07/24 array one

---
+++ arrays/reversedSequence.js
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/5a00e05cc374cb34d100000d
+const reverseSeq = n => {
+    let arr = [];
+     for(let i = n; 0 < i; i--){
+      arr.push(i);
+     }
+    return arr;
+  };
+
+
+//------ Clever of codewars -------
+
+const reverseSeq1 = n => {
+    return Array(n).fill(0).map((e, i) => n - i );
+  };
