373d5bfbe499aa1a2920de89eee33c005ee14dac
carlosbne <106997150+carlosbne@users.noreply.github.com>
2024-10-29 17:30:25 -0300

    added

---
+++ arrays/whatBetween.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/55ecd718f46fba02e5000029
+
+function between(a, b) {
+    let count = []
+    for(i = a; i <= b; i++){
+      count.push(i);
+    }
+    return count;
+  }
