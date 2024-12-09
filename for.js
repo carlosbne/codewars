cc2232f43964a5e457b2c5d5929321e6be4e3d41
carlosbne <106997150+carlosbne@users.noreply.github.com>
2024-12-09 17:47:58 -0300

    if

---
+++ for.js
@@ -0,0 +1,14 @@
+function contador(x){
+    for(let i = 0; i < x; i++){
+        console.log(i);
+    }
+}
+
+function contador(x){
+    for(let i = 0; i < x; i++){
+        if(i == i + x){
+            console.log("aliz");
+        }
+        return 1;
+    }
+}
